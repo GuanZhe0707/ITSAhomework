8527fd742fcded71bbd5afe6df6d1927f4490058
冠哲 王 <4a9g0071@office.stust.edu.tw>
2023-03-15 18:32:14 +0800

    ITSA4

--- ITSAhomework/ITSAhomework.cpp
+++ ITSAhomework/ITSAhomework.cpp
@@ -1,13 +1,25 @@
 ﻿#include <iostream>
-#include<iomanip>
-using namespace  std;
+using namespace std;
 
-int main()
-{
-	int a;
-	double c;
-	cin >> a;
-	c = a * 1.6;
-	cout << fixed << setprecision(1) << c << endl;
+void ParkingCost(int START_HR, int START_MIN, int END_HR, int END_MIN) {
+    int Cost;
+    int TOTAL_ParkingTime = 0, TOTAL_ParkingTime_Half = 0;
+    TOTAL_ParkingTime = ((END_HR * 60) + END_MIN) - ((START_HR * 60) + START_MIN); // min.
 
-} 
+    //TOTAL_ParkingTime_Half = TOTAL_ParkingTime / 30;
+    if (TOTAL_ParkingTime < 120) Cost = 30 * (TOTAL_ParkingTime / 30); // $120
+    else if ((120 <= TOTAL_ParkingTime) && (TOTAL_ParkingTime < 240)) Cost = 120 + (((TOTAL_ParkingTime - 120) / 30) * 40);
+    else if (TOTAL_ParkingTime >= 240) Cost = 120 + 160 + (((TOTAL_ParkingTime - 240) / 30) * 60);
+    else if (TOTAL_ParkingTime < 30) Cost = 0;
+    cout << Cost << endl;
+}
+
+int main() {
+    int START_HR, START_MIN;
+    int END_HR, END_MIN;
+
+    cin >> START_HR >> START_MIN;
+    cin >> END_HR >> END_MIN;
+    ParkingCost(START_HR, START_MIN, END_HR, END_MIN);
+    return 0;
+}
