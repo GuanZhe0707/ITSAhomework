0357fce091d9045f63412d5b23b4237a42470b84
冠哲 王 <4a9g0071@office.stust.edu.tw>
2023-03-15 18:25:10 +0800

    ITSA6

--- ITSAhomework/ITSAhomework.cpp
+++ ITSAhomework/ITSAhomework.cpp
@@ -1,13 +1,51 @@
 ﻿#include <iostream>
-#include<iomanip>
-using namespace  std;
+using namespace std;
 
-int main()
-{
-	int a;
-	double c;
-	cin >> a;
-	c = a * 1.6;
-	cout << fixed << setprecision(1) << c << endl;
-
-} 
+int main() {
+    int season;
+    while (cin >> season)
+    {
+        switch (season)
+        {
+        case 1:
+            cout << "Winter" << endl;
+            break;
+        case 2:
+            cout << "Winter" << endl;
+            break;
+        case 3:
+            cout << "Spring" << endl;
+            break;
+        case 4:
+            cout << "Spring" << endl;
+            break;
+        case 5:
+            cout << "Spring" << endl;
+            break;
+        case 6:
+            cout << "Summer" << endl;
+            break;
+        case 7:
+            cout << "Summer" << endl;
+            break;
+        case 8:
+            cout << "Summer" << endl;
+            break;
+        case 9:
+            cout << "Autumn" << endl;
+            break;
+        case 10:
+            cout << "Autumn" << endl;
+            break;
+        case 11:
+            cout << "Autumn" << endl;
+            break;
+        case 12:
+            cout << "Winter" << endl;
+            break;
+        default:
+            break;
+        }
+    }
+    return 0;
+}
