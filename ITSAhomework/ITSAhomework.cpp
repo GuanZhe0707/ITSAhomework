d7935a8a624faa4e254b1ec8f32908d89abc5add
冠哲 王 <4a9g0071@office.stust.edu.tw>
2023-03-15 18:19:05 +0800

    加入專案檔案。

---
+++ ITSAhomework/ITSAhomework.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include<iomanip>
+using namespace  std;
+
+int main()
+{
+	int a;
+	double c;
+	cin >> a;
+	c = a * 1.6;
+	cout << fixed << setprecision(1) << c << endl;
+
+}
