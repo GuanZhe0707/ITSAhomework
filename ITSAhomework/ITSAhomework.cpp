aab3f50f63f0671e9fb362837352f9787dcd5bdf
冠哲 王 <4a9g0071@office.stust.edu.tw>
2023-03-15 18:57:50 +0800

    ITSA3

--- ITSAhomework/ITSAhomework.cpp
+++ ITSAhomework/ITSAhomework.cpp
@@ -1,50 +1,17 @@
-﻿#include <iostream>
+﻿#include <iostream>  
 using namespace std;
 
 int main() {
-    int season;
-    while (cin >> season)
+    int x, y;
+    while (cin >> x >> y)
     {
-        switch (season)
+        if (x * x + y * y <= 10000)
         {
-        case 1:
-            cout << "Winter" << endl;
-            break;
-        case 2:
-            cout << "Winter" << endl;
-            break;
-        case 3:
-            cout << "Spring" << endl;
-            break;
-        case 4:
-            cout << "Spring" << endl;
-            break;
-        case 5:
-            cout << "Spring" << endl;
-            break;
-        case 6:
-            cout << "Summer" << endl;
-            break;
-        case 7:
-            cout << "Summer" << endl;
-            break;
-        case 8:
-            cout << "Summer" << endl;
-            break;
-        case 9:
-            cout << "Autumn" << endl;
-            break;
-        case 10:
-            cout << "Autumn" << endl;
-            break;
-        case 11:
-            cout << "Autumn" << endl;
-            break;
-        case 12:
-            cout << "Winter" << endl;
-            break;
-        default:
-            break;
+            cout << "inside" << endl;
+        }
+        else
+        {
+            cout << "outside" << endl;
         }
     }
     return 0;
