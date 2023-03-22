af827dadaf989f1d3d37b466854d3df631099f0b
冠哲 王 <4a9g0071@office.stust.edu.tw>
2023-03-22 21:44:46 +0800

    ITSA10

--- ITSAhomework/ITSAhomework.cpp
+++ ITSAhomework/ITSAhomework.cpp
@@ -1,80 +1,32 @@
-﻿#include <iostream>
+﻿#include<iostream>
 using namespace std;
-
-class Complex {
-private:
-    int re, im;
-public:
-    Complex(int r = 0, int i = 0) {
-        re = r;
-        im = i;
-    }
-
-    Complex operator + (Complex const& obj) {
-        Complex result;
-        result.re = re + obj.re;
-        result.im = im + obj.im;
-        return result;
-    }
-
-    Complex operator - (Complex const& obj) {
-        Complex result;
-        result.re = re - obj.re;
-        result.im = im - obj.im;
-        return result;
-    }
-
-    Complex operator * (Complex const& obj) {
-        Complex result;
-        result.re = re * obj.re - im * obj.im;
-        result.im = re * obj.im + obj.re * im;
-        return result;
-    }
-
-    void display() {
-        cout << re << "  " << im << endl;
-    }
-};
 
 int main()
 {
-    //Complex c;
-    //c.display();
+    int a, b;
+    while (cin >> a >> b)
+    {
+        while (a != 0 and b != 0)
+        {
+            if (a >= b)
+            {
+                a = a % b;
+            }
+            else if (b > a)
+            {
+                b = b % a;
+            }
+        }
 
-    //Complex c1(2,3);
-    //c1.display();
-
-    //Complex c2(5, -7);
-    //c2.display();
-
-    //Complex c3 = c1 + c2;
-    //c3.display();
-
-    int n;
-    cin >> n;
-
-    Complex c[10];
-    for (int i = 1; i <= n; i++) {
-        char oper;
-        int ar, ai, br, bi;
-        cin >> oper >> ar >> ai >> br >> bi;
-        Complex a(ar, ai);
-        Complex b(br, bi);
-
-        switch (oper) {
-        case '+':
-            c[i] = a + b;
-            break;
-        case  '-':
-            c[i] = a - b;
-            break;
-        case  '*':
-            c[i] = a * b;
-            break;
+        if (a >= b)
+        {
+            cout << a << endl;
+        }
+        else
+        {
+            cout << b << endl;
         }
     }
-    for (int i = 0; i < n; i++) {
-        c[i].display();
-    }
 
+    return 0;
 }
