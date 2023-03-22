4fb8703f772647be550b3e671b55a08359f28aa8
冠哲 王 <4a9g0071@office.stust.edu.tw>
2023-03-22 19:22:31 +0800

    ITSA9

--- ITSAhomework/ITSAhomework.cpp
+++ ITSAhomework/ITSAhomework.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-	int num, count = 0;
+	int N, sum = 0;
 
-	cin >> num;
-	for (int i = 2; i < num; i++)
-		if (num % i == 0) count = count + 1;
-	if (count == 0)
-		cout << "YES" << endl;
-	else cout << "NO" << endl;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		if (i % 3 == 0) sum += i;
+	}
+	cout << sum << endl;
 	return 0;
 }
+
