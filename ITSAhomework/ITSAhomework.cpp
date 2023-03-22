9309458a4111f4aaf9e7fd38cf7c99cfee1175c0
冠哲 王 <4a9g0071@office.stust.edu.tw>
2023-03-22 19:19:45 +0800

    ITSA8

--- ITSAhomework/ITSAhomework.cpp
+++ ITSAhomework/ITSAhomework.cpp
@@ -1,61 +1,14 @@
-﻿#include<iostream>
-#include<string>
-#include<sstream>
-#include<vector>
+﻿#include <iostream>
 using namespace std;
 
-void sortPoke(vector<string>& a, int n);
-int main()
-{
-	int input = 0, n = 0;
-	string Str, s;
-	vector<string> pokeS;
+int main() {
+	int num, count = 0;
 
-	cin >> input;
-	cin.ignore(1, '\n');
-	for (int i = 0; i < input; i++)
-	{
-		getline(cin, Str);
-		istringstream cutStr(Str);
-		while (getline(cutStr, s, ' '))
-		{
-			pokeS.push_back(s);
-		}
-		sortPoke(pokeS, pokeS.size());
-		for (int j = 0; j < pokeS.size(); j++)
-		{
-			if (j == pokeS.size() - 1) cout << pokeS[j] << endl;
-			else cout << pokeS[j] << " ";
-		}
-		pokeS.clear();
-	}
+	cin >> num;
+	for (int i = 2; i < num; i++)
+		if (num % i == 0) count = count + 1;
+	if (count == 0)
+		cout << "YES" << endl;
+	else cout << "NO" << endl;
 	return 0;
-}
-void sortPoke(vector<string>& a, int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			string t;
-			if (a[i][0] > a[j][0]) { t = a[i]; a[i] = a[j]; a[j] = t; }
-		}
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0 + i; j < n; j++)
-		{
-			if (a[i][0] == a[j][0])
-			{
-				string t, bN = a[i].substr(1, 2), sN = a[j].substr(1, 2);
-				istringstream bigN(bN);
-				istringstream smaN(sN);
-				int b = 0, s = 0;
-				bigN >> b; smaN >> s;
-				if (b < s) { t = a[i]; a[i] = a[j]; a[j] = t; }
-			}
-		}
-	}
-	
 }
